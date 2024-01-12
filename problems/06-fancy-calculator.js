ab0642831e43a2928a8203a629fec03259cfc8ae
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-12 12:35:25 -0500

    complete

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,22 @@
+const Calculator = require("./02-calculator");
+
 // Your code here
+class FancyCalculator extends Calculator {
+	setTotal = (newTotal) => this.total = newTotal;
+
+	modulo = (num) => this.total %= num;
+
+	squared = () => this.total ** 2;
+}
+
+let fancyCalculator = new FancyCalculator();
+console.log(fancyCalculator.setTotal(5)); // => 5
+console.log(fancyCalculator.squared());   // => 25
+console.log(fancyCalculator.modulo(4));   // => 1
+console.log(fancyCalculator.total);        // => 1
+
+// can use instance methods on the Calculator class
+console.log(fancyCalculator.add(9));       // => 10
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,29 @@
 // Your code here
+class PartyPlanner {
+	constructor(guestList = []) {
+		this.guestList = guestList;
+	}
 
+	addToGuestList = (name) => this.guestList.push(name);
+
+	throwParty = () => this.guestList.length === 0 ? 'Gotta add people to the guest list' : `Welcome to the party ${this.guestList.join(' and ')}`;
+}
+
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
+
+const party2 = new PartyPlanner();
+console.log(party2.throwParty()); // prints "Gotta add people to the guest list"
+
+party2.addToGuestList("Lucy");
+console.log(party2.throwParty()); // prints "Welcome to the party Lucy"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
